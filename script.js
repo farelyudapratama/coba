c7c05a05caee1c92e32e21d3f0c7dd647457e504
farelyudapratama <dhakayudha2@gmail.com>
2024-02-26 23:03:23 +0700

    up

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const showOn = document.getElementById('addBook');
-const showOff = document.getElementById('close');
-const overlay = document.getElementById('formBook');
+const showOn = document.querySelector('.addBook Button');
+const overlay = document.querySelector('.formBook');
 
 showOn.addEventListener('click', function () {
-    clear();
     overlay.style.display = 'flex';
-    const h2 = document.querySelector('.input_section .title-input h2');
-    h2.innerText = 'Add New Book'
-
-    const editNameButton = document.getElementById('bookSubmit');
-    editNameButton.innerText = 'Add Book';
 });
 
-showOff.addEventListener('click', function () {
-    overlay.style.display = 'none';
-    selectedEditBook.length = 0;
+document.addEventListener('click', function (event) {
+    const targetElement = event.target;
+    if (!targetElement.closest('.input_section') && !targetElement.matches('.addBook Button')) {
+        overlay.style.display = 'none';
+        history.replaceState(null, null, 'index.html');
+    }
 });
